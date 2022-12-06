18d2c263c5aec081ff863e555a05484579d7570d
fmghk1 <hazel.kim@fmg.co.nz>
2022-12-07 12:42:02 +1300

    Update configs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,10 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    'storybook-addon-next',
+    'storybook-addon-designs',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   "framework": "@storybook/react",
   "core": {
---
+++ stories/pages/home.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Home from '../../pages/index'
+
+export default {
+  title: 'Pages',
+  component: Home
+} as ComponentMeta<typeof Home>
+
+export const HomePage = () => <Home />
+
+//
+// const Template: ComponentStory<typeof Home> = (args) => <Home {...args} />
+//
+// export const Default = Template.bind({})
