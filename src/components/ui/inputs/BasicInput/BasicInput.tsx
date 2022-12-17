f4bd3ea5b3cbaf28e819024ea30d8f512ea0a8ea
fmghk1 <hazel.kim@fmg.co.nz>
2022-12-17 16:04:28 +1300

    Add invalid icon

--- src/components/ui/inputs/BasicInput/BasicInput.tsx
+++ src/components/ui/inputs/BasicInput/BasicInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import InputUnstyled, { InputUnstyledProps } from '@mui/base/InputUnstyled';
+import InvalidIcon from '@/src/assets/icons/18x18/invalid.svg';
 import { IInputProps } from '../input-types';
 
 export type BasicInputProps = InputUnstyledProps & IInputProps;
@@ -15,14 +16,18 @@ export const BasicInput = React.forwardRef(function CustomInput(
         input: {
           'aria-label': props['aria-label'] ?? label ?? props.name,
           pattern,
-          className: `text-base bg-base-1 hover:bg-base-2 rounded outline active:outline-1 focus:outline-1 p-3 ${
-            error ? 'outline-1 outline-error' : 'outline-0 outline-fmg-green'
-          } ${width} ${className}`,
+          className: `bg-transparent outline-0 p-3 ${width}`,
         },
       }}
       {...props}
       ref={ref}
       error={error}
+      // TODO: theme with mui config
+      className={`flex text-base bg-base-1 hover:bg-base-2 rounded outline active:outline-1 focus-within:outline-1 ${
+        error ? 'outline-1 outline-error' : 'outline-0 outline-fmg-green'
+      } ${width} ${className}`}
+      // TODO: fill color not working
+      endAdornment={error ? <InvalidIcon className="self-center fill-error mx-[-24px]" /> : null}
     />
   );
 });
